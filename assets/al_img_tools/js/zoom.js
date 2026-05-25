1b37536562a8949117630da61f21d2a2770db750
alshedivat <alshedivat@users.noreply.github.com>
2026-05-25 19:47:55 +0000

    Deploying to gh-pages from @ alshedivat/al-folio@ac7add47defd381e997251e5404f1ea177728ad3 🚀

--- assets/al_img_tools/js/zoom.js
+++ assets/al_img_tools/js/zoom.js
@@ -1,6 +1,9 @@
-// Initialize medium zoom.
-$(document).ready(function () {
-  medium_zoom = mediumZoom("[data-zoomable]", {
-    background: getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color") + "ee", // + 'ee' for trasparency.
+document.addEventListener("DOMContentLoaded", () => {
+  if (typeof mediumZoom !== "function") {
+    return;
+  }
+  mediumZoom("[data-zoomable]", {
+    // Append alpha to theme background color to keep content visible under zoom.
+    background: `${getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color")}ee`,
   });
 });
